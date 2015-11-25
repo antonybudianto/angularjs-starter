45bad9970eca9cedd0844bc166dd783f0844f1fd
Antony <antony.budianto@gdplabs.id>
2015-11-25 13:59:24 +0700

    Add auth run unit test

---
+++ src/client/app/core/auth/test/auth.run.spec.js
@@ -0,0 +1,68 @@
+/* jshint -W117 */
+describe('app.core.auth - run', function() {
+    var state;
+    var toastr;
+    var authService;
+    var rootScope;
+
+    beforeEach(function () {
+        module('app.core.auth', function($provide) {
+            toastr = {
+                error: jasmine.createSpy(),
+                info: jasmine.createSpy(),
+                warning: jasmine.createSpy(),
+                success: jasmine.createSpy()
+            };
+            $provide.value('toastr', toastr);
+        });
+
+        inject(function($injector) {
+            rootScope = $injector.get('$rootScope');
+            state = $injector.get('$state');
+            authService = $injector.get('authService');
+        });
+    });
+
+    it('should be defined', function() {
+        expect(authService).toBeDefined();
+    });
+
+    describe('stateChangeStart', function() {
+        it('should go to afterLogin after success login', function() {
+            spyOn(authService, 'isAuth').and.returnValue(true);
+            spyOn(state, 'go');
+            rootScope.$emit('$stateChangeStart', {},
+            {
+                afterLogin: 'dashboard'
+            });
+            expect(state.go).toHaveBeenCalledWith('dashboard');
+        });
+
+        it('should not go to afterLogin if not defined', function() {
+            spyOn(authService, 'isAuth').and.returnValue(true);
+            spyOn(state, 'go');
+            rootScope.$emit('$stateChangeStart', {}, {});
+            expect(state.go).not.toHaveBeenCalled();
+        });
+
+        describe('loginRequired true', function () {
+            it('should not go to login if already login', function () {
+                spyOn(authService, 'isAuth').and.returnValue(true);
+                spyOn(state, 'go');
+                rootScope.$emit('$stateChangeStart', {
+                    loginRequired: true
+                }, {});
+                expect(state.go).not.toHaveBeenCalled();
+            });
+
+            it('should go to login if not login', function () {
+                spyOn(authService, 'isAuth').and.returnValue(false);
+                spyOn(state, 'go');
+                rootScope.$emit('$stateChangeStart', {
+                    loginRequired: true
+                }, {});
+                expect(state.go).toHaveBeenCalled();
+            });
+        });
+    });
+});
